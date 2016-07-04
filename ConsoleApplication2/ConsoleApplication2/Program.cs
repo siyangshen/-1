d9e551b79ef87effa462fac861da7cb07d746e76
siyangshen <siyang.shen@diva.co.jp>
2016-07-04 18:12:06 +0900

    演習１の解答

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        string temp;
+        int price;
+
+        System.Console.Write("価格を入力してください>>>");
+        temp = System.Console.ReadLine();
+        price = System.Int32.Parse(temp);
+
+        System.Console.WriteLine("税込み価格{0:c}", price * 1.05);
+    }
+}
---
+++ ConsoleApplication2/演習２/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        string temp;
+        int height;
+
+        System.Console.Write("身長[cm]を入力してください>>>");
+        temp = System.Console.ReadLine();
+        price = System.Int32.Parse(temp);
+
+        System.Console.WriteLine("税込み価格{0:c}", price * 1.05);
+    }
+}
