84c2a16d22d2822ad45922f99639baa640ddd9f2
siyangshen <siyang.shen@diva.co.jp>
2016-07-05 10:42:48 +0900

    演習３の解答

---
+++ 演習３/演習３/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0 | i % 7 == 0)
+            {
+                continue;
+            }
+            Console.Write(i+ " ");
+        }
+    }
+}
