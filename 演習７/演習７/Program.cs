ad148d29d194a88433d3678da052fc5d76d8caf5
siyangshen <siyang.shen@diva.co.jp>
2016-07-06 16:38:25 +0900

    演習８の解答

---
+++ 演習７/演習７/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Computer
+{
+    public static int daisu;
+    public string ComputerName;
+    public string InstalledOS;
+    public int memory;
+    public int disk;
+
+   
+}
+
+
+
+class MainClass
+{ 
+    static void Main()
+    {
+        Computer.daisu = 0;
+        Console.WriteLine("現在の台数={0}",Computer.daisu);
+        Computer computer1 = new Computer();
+        computer1.ComputerName = "Sales01";
+        computer1.InstalledOS = "Windows 8 Ultimate";
+        computer1.memory = 1000;
+        computer1.disk = 0;
+        Computer computer2 = new Computer();
+        computer2.ComputerName = "Sales02";
+        computer2.InstalledOS = "Windows 7 Professional";
+        computer2.memory = 1600;
+        computer2.disk = 120;
+        Computer.daisu = Computer.daisu+2;
+
+        Console.WriteLine("現在の台数={0}", Computer.daisu);
+        Console.WriteLine("{0}, {1}", computer1.ComputerName, computer1.InstalledOS);
+        Console.WriteLine("Memory={0}MB,Disk={1}GB", computer1.memory, computer1.disk);
+        Console.WriteLine("{0},{1}", computer2.ComputerName, computer2.InstalledOS);
+        Console.WriteLine("Memory={0}MB,Disk={1}GB", computer2.memory, computer2.disk);
+
+    }
+}
---
+++ 演習８/演習８/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Auto
+{
+    private int displacement;
+    protected double fuel;
+    public Auto(int d)
+    {
+        displacement = d;
+    }
+    public void AddFuel(double f)
+    {
+        fuel = f;
+    }
+    public int GetDisplacement()
+    {
+        return displacement;
+    }
+}
+class Sedan:Auto
+{
+    public Sedan(int d):base(d)
+    {
+
+    }
+
+}
+class Truck:Auto
+{
+    public Truck(int d):base(d)
+    {
+
+    }
+}
+class MainClass
+{
+    static void Main()
+    {
+        Sedan auto1 = new Sedan(2000);
+        auto1.AddFuel(50);
+        Console.WriteLine("排気量{0}ccの車", auto1.GetDisplacement());
+        Truck auto2 = new Truck(5500);
+        auto2.AddFuel(50);
+        Console.WriteLine("排気量{0}ccのトラック", auto2.GetDisplacement());
+    }
+}
